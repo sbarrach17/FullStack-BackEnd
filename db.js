fc68084dcc9634e5efdb250882cd5d564d62ce23
Sebastián Barra <s.andresbarra@gmail.com>
2024-06-30 22:17:31 -0400

    SUBIDA DATABASE

--- db.js
+++ db.js
@@ -2,11 +2,19 @@ const { Pool } = require('pg');
 require('dotenv').config(); // Carga las variables de entorno desde .env
 
 const pool = new Pool({
+    connectionString: proccss.env.DATABASE_URL,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    allowExitOnIdle: true
+    port: process.env.DB_PORT || 5432,
+  });
+pool.connect((error) => {
+    if (error) {
+        throw new error (error);
+    } else {
+        console.log ('DB conectada');
+    }
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { Pool } = require("pg"); // Import Pool from pg module
 const cors = require("cors");
 const userRoutes = require("./routes/user.Routes.js");
 
@@ -9,12 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl:{
-    rejectUnauthorized: false
-  }
-})
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
