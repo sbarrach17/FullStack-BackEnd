3048bb9d0621a54a59c5f5876229d4af5b7af076
Sebastian Barra <sbarra@quilicura.local>
2024-07-01 13:49:57 -0400

    actualizado

--- db.js
+++ db.js
@@ -2,20 +2,23 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    // connectionString: process.env.DATABASE_URL,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT || 5432,
-});
-
-pool.connect((error) => {
-    if (error) {
-        throw new Error(error.message); // Throw a new Error object with the error message
-    } else {
-        console.log('DB conectada');
+    ssl: {
+        rejectUnauthorized: false 
     }
 });
+
+// pool.connect((error) => {
+//     if (error) {
+//         throw new Error(error.message); 
+//     } else {
+//         console.log('DB conectada');
+//     }
+// });
 
 module.exports = pool;
