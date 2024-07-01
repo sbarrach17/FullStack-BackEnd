f281bfe0b527657139fc4e0a044931b39780b6ca
Sebastián Barra <s.andresbarra@gmail.com>
2024-06-30 22:43:37 -0400

    SUBIDA DATABASE

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const userRoutes = require("./routes/user.Routes.js");
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(userRoutes);
 
-
+const corsOptions = {
+  origin: 'https://fullstack-frontend-x1zs.onrender.com', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+  allowedHeaders: 'Content-Type,Authorization'
+};
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
